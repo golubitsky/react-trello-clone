80fc904ab1ccb4049183594a27bd4846294edf13
Mike Golubitsky <mike617@gmail.com>
2017-08-27 00:24:22 -0700

    update heroku script

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 export HEROKU_DEBUG=1  
-heroku plugins:install heroku-container-registry
+if [ heroku plugins:install heroku-container-registry ]; then
+    echo "installed heroku-container-registry"
+else
+    echo "FAILED to install heroku-container-registry"
+    heroku version
+    exit 1
+fi
 docker login -u $DOCKER_USER -p $DOCKER_PASS --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
